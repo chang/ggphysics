a9fa1f9b4a937e4f7cbb397bd4ff0768a38eea2a
Eric <ericchang00@gmail.com>
2017-11-05 12:07:34 -0800

    install packages and create directory to store frames if needed

--- util.R
+++ util.R
@@ -1,5 +1,22 @@
 #' General utilities for creating physics-based animations in ggplot2
 
+#' Installs packages if needed
+install_required_packages = function() {
+  required = c('ggplot2', 'dplyr', 'purrr', 'glue')
+  for (pkg in required) {
+    if (!(pkg %in% installed.packages())) {
+      install.packages(pkg)
+    }
+  }
+}
+
+
+install_required_packages()
+library(ggplot2)
+library(dplyr)
+library(purrr)
+
+
 #' Returns a random color as a string
 random_color = function() {
   COLORS = c('firebrick', 'skyblue', 'green', 'purple', 'yellow', 'blue', 'red')
@@ -41,8 +58,22 @@ theme_physics = function() {
 }
 
 
+#' Grabs the directory path from a file path
+dir_path = function(path) {
+  sep = file.path("", "")  # gets file path separator in an OS agnostic way
+  directory = strsplit(path, sep) %>% .[[1]] %>% .[-length(.)] %>% as.list()
+  do.call(file.path, directory)
+}
+
+
 #' Save and print message
 save_frame = function(plt, path, i) {
+  dir_ = dir_path(path)
+  if (!dir.exists(dir_)) {
+    message(glue::glue("Creating directory at: {dir_}", dir_ = dir_))
+    dir.create(dir_)
+  }
+
   fpath = glue::glue(path, i=i)
   ggsave(filename = fpath, plot = plt, width = 1.5, height = 1.5)
   message(fpath)
